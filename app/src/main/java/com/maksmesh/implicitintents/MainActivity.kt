f1dd0a5ff6a0c3d85ff1db57ef8bf04e5018031a
Maxim Meshcheryakov <25-nv-86@mail.ru>
2022-11-06 18:31:29 +0600

    I did all functionality (open web, open location, share text)

--- app/src/main/java/com/maksmesh/implicitintents/MainActivity.kt
+++ app/src/main/java/com/maksmesh/implicitintents/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.maksmesh.implicitintents
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 import com.maksmesh.implicitintents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        openWebSite()
+        openLocation()
+        shareText()
+    }
+
+    private fun openWebSite() = with(binding) {
+        openWebsiteBTN.setOnClickListener {
+            val url = webSiteEditText.text.toString()
+            val webpage = Uri.parse("https://$url")
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(intent)
+        }
+    }
+
+    private fun openLocation() = with(binding) {
+        locationBTN.setOnClickListener {
+            val location = locationEditText.text.toString()
+            val addressUri = Uri.parse("geo:0,0?q=$location")
+            val intent = Intent(Intent.ACTION_VIEW, addressUri)
+            startActivity(intent)
+        }
+    }
+
+    private fun shareText() = with(binding) {
+        shareBTN.setOnClickListener {
+            val txt = shareEditText.text.toString()
+            val mimeType = "text/plain"
+            ShareCompat
+                .IntentBuilder(this@MainActivity)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_text_header)
+                .setText(txt)
+                .startChooser()
+        }
     }
 }
